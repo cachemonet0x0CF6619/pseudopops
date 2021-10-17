1b0c5f0b5c80bfb1380827ee77e50f7ac423c036
cachemonet0x0CF6619 <90769841+cachemonet0x0CF6619@users.noreply.github.com>
2021-10-17 14:45:04 -0500

    feat: deploy initial subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -8,28 +8,18 @@ import {
   SodaPoured,
   Transfer
 } from "../generated/Pseudopops/Pseudopops"
-import { ExampleEntity } from "../generated/schema"
+
+import { Soda, Collection } from "../generated/schema";
 
 export function handleApproval(event: Approval): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Soda.load(event.transaction.from.toHex())
+  if (!entity) return;
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
   entity.owner = event.params.owner
-  entity.approved = event.params.approved
 
   // Entities can be written to the store with `.save()`
   entity.save()
@@ -80,10 +70,32 @@ export function handleApproval(event: Approval): void {
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleNameChanged(event: NameChanged): void {}
+export function handleNameChanged(event: NameChanged): void {
+  let soda = Soda.load(event.transaction.from.toHex())
+  if (!soda) return;
+
+  soda.name = event.params.newName;
+  soda.save();
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleSodaPoured(event: SodaPoured): void {}
+export function handleSodaPoured(event: SodaPoured): void {
+  let soda = Soda.load(event.transaction.from.toHex())
+  if (!soda) return;
+  soda.owner = event.transaction.from;
+  soda.tokenId = event.params.tokenId;
+  soda.poured = event.block.timestamp;
+  soda.lastPoured = soda.poured;
+  soda.save();
+}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let soda = Soda.load(event.transaction.from.toHex())
+  if (!soda) return;
+  soda.owner = event.params.to;
+  soda.tokenId = event.params.tokenId;
+  soda.poured = event.block.timestamp;
+  soda.lastPoured = soda.poured;
+  soda.save();
+}
